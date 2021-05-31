06e265694fb32b936dd450ab2f497ad19320d66e
yukihiro.namba <akauntodemo7@gmail.com>
2021-05-31 08:05:45 +0000

    saiteisyutsu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,11 +50,21 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render"edit"
+    else
+      redirect_to root_url
+    end
   end
-
+  
   def followings
     @user = User.find(params[:id])
     @followings = @user.followings.page(params[:page])
+    if @user == current_user
+      render "followings"
+    else
+      redirect_to root_url
+    end
   end
   
   def followers
