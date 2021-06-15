9693a70720a0e0be25909ed1f2e334abb5aca51f
yukihiro.namba <akauntodemo7@gmail.com>
2021-06-15 08:12:23 +0000

    user tsuika

--- app/models/user.rb
+++ app/models/user.rb
@@ -70,9 +70,9 @@ class User < ApplicationRecord
     # SNS認証を行ったことがあるかを判断して、データベースに保存
     
     user = User.where(email: auth.info.email).first_or_initialize(
-         name: auth.info.name,
-         email: auth.info.email,
-         password: Devise.friendly_token[0, 20]
+      user.name= auth.info.name,
+      user.email = auth.info.email,
+      user.password = Devise.friendly_token[0,20]
      )
     # SNS認証を行っていなかった場合、メールアドレスで検索
   
