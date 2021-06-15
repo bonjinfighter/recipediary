b96c7fc21ebc086354d04fbe4c424760fbb9e757
yukihiro.namba <akauntodemo7@gmail.com>
2021-06-15 04:27:10 +0000

    facebook2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+         :recoverable, :rememberable, :validatable, 
+         :omniauthable, omniauth_providers: %i[facebook]
 
   has_many :sns_credentials
   
