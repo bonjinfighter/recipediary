82ab15d2a467c40d208c26671b7c8289ab639fe1
yukihiro.namba <akauntodemo7@gmail.com>
2021-06-15 07:36:09 +0000

    add

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,8 +72,7 @@ class User < ApplicationRecord
     user = User.where(email: auth.info.email).first_or_initialize(
          name: auth.info.name,
          email: auth.info.email,
-         password: pass=Devise.friendly_token[0, 20],
-         password_confirmation: pass
+         password: Devise.friendly_token[0, 20]
      )
     # SNS認証を行っていなかった場合、メールアドレスで検索
   
